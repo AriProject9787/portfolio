61c50234984207124a126f63e3a65782ed7bff42
Arirama Selvam M <ariofficial9787@gmail.com>
2026-03-30 11:43:09 +0530

    secure api key

---
+++ js/firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase configuration for the portfolio
+// WARNING: This file contains an API Key. Ensure it is restricted in the Google Cloud/Firebase console.
+const firebaseConfig = {
+  apiKey: "AIzaSyAB1-fJkHg1T2RmyF1vmjK8ddTCdoPHLdU",
+  authDomain: "userdata-9787.firebaseapp.com",
+  projectId: "userdata-9787",
+  storageBucket: "userdata-9787.firebasestorage.app",
+  messagingSenderId: "719249234099",
+  appId: "1:719249234099:web:8a3da8fa6a3fa70f6578c6",
+  measurementId: "G-MFKSWD16N1"
+};
+
+export default firebaseConfig;
